44d2ca33336729176c9be1b2bf330cb6642db899
Tomaz Leme <tomazleme@hotmail.com>
2020-06-30 14:52:14 -0300

    Laravelcms1.0

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+
+class LoginController extends Controller 
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+   // protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/painel';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function index(){
+        return view('admin.login');
+    }
+    public function authenticate(Request $request) {
+         $data = $request->only([
+             'email',
+             'password',
+             'remember'
+         ]);
+      $validator = $this->validator($data);
+
+         if($validator)->fails()) {
+             return redirect()->route('login')
+             ->withErrors($validator)
+             ->withInput();
+         }
+         if(Auth::attempt($data)) {
+                return redirect()->route('admin');
+           }else {
+                return redirect()->route('login');
+
+           } 
+        
+    }
+    public function logout() {
+      Auth::logout();
+      return redirect()->route('auth.login');
+
+    }
+    protected function validator(array $data) {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:150'],
+            'password' => ['required', 'string', 'min:4']
+        ]);
+        
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+<h1>Painel de Controle</h1>
+@endsection
+
+@section('content')
+............
+@endsection
+
+
